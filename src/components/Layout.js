b2f118860ad797c8969303f40f09136480411064
dnsfo159 <dnsfo159@naver.com>
2021-03-17 15:35:01 +0900

    test입니다.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,15 +7,13 @@ import { pullRight, h1 } from './Lasyout.css';
 const Layout = ({ children }) => {
     return (
         <Container>
-            <Link to="/">
-                <Header as="h1" className={h1}>
-                    webpack-for-react
-                </Header>
-            </Link>
+            <Header as="h1" className={h1}>
+                webpack-for-react header 면 header 지 왜 링크가있는겨?
+            </Header>
             {children}
             <Divider />
             <p className={pullRight}>
-                Made with <Icon name="heart" color="red" /> by Esau Silva
+                Made with <Icon name="heart" color="blue" /> by Esau Silva
             </p>
         </Container>
     );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
+import App from './components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
-//AWS 클라이언트 붙히기..
-// webpack 설정후 데이터 입히기
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import aws_config from './aws-exports';
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter } from 'react-router-dom'
 
+import Amplify from 'aws-amplify';
 import config from './aws-exports';
+
+Amplify.configure(config);
+
+const client = new AWSAppSyncClient({
+    url: aws_config.aws_appsync_graphqlEndpoint,
+    region: aws_config.aws_appsync_region,
+    auth: {
+        type: AUTH_TYPE.API_KEY,
+        apiKey: aws_config.aws_appsync_apiKey,
+    },
+    Storage: {
+        AWSS3: {
+            bucket: 'oniontoon-20210316171844-hostingbucket-dev', //REQUIRED -  Amazon S3 bucket name public?
+            region: aws_config.aws_appsync_region
+        }
+    }
+});
 
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
-);
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+)
